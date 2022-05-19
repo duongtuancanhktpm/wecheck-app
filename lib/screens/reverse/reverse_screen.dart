ae604e4f2a0aec14873279116061a0775f794626
minhnv <minhnv@kaopiz.com>
2022-05-19 08:39:59 +0700

    create reverse screen

---
+++ lib/screens/reverse/reverse_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReverseScreen extends StatelessWidget {
+  const ReverseScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red,);
+  }
+}
